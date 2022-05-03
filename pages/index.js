90a5e0a833cd5226cf7c8b48fae09ceccc1e117c
ilosrim <ilosrim@yahoo.com>
2022-05-03 05:17:40 +0500

    add theme, dark-white

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import { Container, Box, Heading } from "@chakra-ui/react"
+import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react"
 
 const Page = ()=> {
 	return (
 		<Container>
-			<Box borderRadius="lg" bg="red" mb={6} p={3} alignContent="center">
+			<Box 
+				borderRadius="lg" 
+				bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} 
+				mb={6} p={3} 
+				alignContent="center"
+			>
 				Hello, I&apos;m a full-stack developer based in uzbekistan!
 			</Box>
 
@@ -20,7 +25,16 @@ const Page = ()=> {
 					ml={{md: 6}}
 					align="center"
 				>
-					<Image />
+					<Image 
+						borderColor="whiteAlpha.800" 
+						borderWidth={2} 
+						borderStyle="solid" 
+						maxWidth="100px" 
+						display="inline-block" 
+						borderRadius="full" 
+						src="/images/takuya.jpg" 
+						alt="Profile image" 
+					/>
 				</Box>
 			</Box>
 		</Container>
