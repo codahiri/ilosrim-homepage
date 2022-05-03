a13380cf4479c36b251ef3b52531f49640b2f2a8
ilosrim <ilosrim@yahoo.com>
2022-05-03 10:06:36 -0400

    add new photo, add works & bio section

--- components/section.js
+++ components/section.js
@@ -7,7 +7,7 @@ const StyledDiv = chakra(motion.div, {
   }
 })
 
-const Section = ({ children, delay=0}) => {
+const Section = ({ children, delay=0}) => (
   <StyledDiv 
     initial={{y: 10, opacity: 0}}
     animate={{y: 0, opacity: 1}}
@@ -15,4 +15,6 @@ const Section = ({ children, delay=0}) => {
     mb={6}>
       {children}
   </StyledDiv>
-}
+)
+
+export default Section
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,17 @@
-import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react"
+import NextLink from 'next/link'
+import { 
+	Container, 
+	Box, 
+	Heading, 
+	Image, 
+	useColorModeValue,
+	Button
+} from "@chakra-ui/react"
+import Paragraph from "../components/paragraph"
+import Section from "../components/section"
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { BioSection, BioYear } from '../components/bio'
+import Link from 'next/link'
 
 const Page = ()=> {
 	return (
@@ -9,7 +22,7 @@ const Page = ()=> {
 				mb={6} p={3} 
 				alignContent="center"
 			>
-				Hello, I&apos;m a full-stack developer based in uzbekistan!
+				Hello, I&apos;m a full-stack developer based in Uzbekistan!
 			</Box>
 
 			<Box display={{md:'flex'}}>
@@ -32,11 +45,60 @@ const Page = ()=> {
 						maxWidth="100px" 
 						display="inline-block" 
 						borderRadius="full" 
-						src="/images/takuya.jpg" 
+						src="/images/ilosrim.png" 
 						alt="Profile image" 
 					/>
 				</Box>
 			</Box>
+
+			<Section delay={0.1}>
+				<Heading as="h1" variant="section-title">
+					Work
+				</Heading>
+				<Paragraph>
+					A programmer in the field of frontend web and telegram bot production, 
+					has +1 years of experience. Creator of e-commerce, single-page and multi-page, 
+					as well as admin panel websites. Specializes in project implementation and management. 
+					An inquisitive programmer thirsting to learn new knowledge ;)
+				</Paragraph>
+				<Box align="center" my={4}>
+					<NextLink href="/works">
+						<Button rightIcon={<ChevronRightIcon />}>
+							My portfolio
+						</Button>
+					</NextLink>
+				</Box>
+			</Section>
+
+			<Section delay={0.2}>
+				<Heading as="h1" variant="section-title">
+					Bio
+				</Heading>
+				<BioSection>
+					<BioYear>1998</BioYear>
+					Born in Fergana, Uzbekistan
+				</BioSection>
+				<BioSection>
+					<BioYear>2022</BioYear>
+					Completed the Bachelor&apos;s degree in the Biology at National universitet of Uzbekistan
+				</BioSection>
+				<BioSection>
+					<BioYear>2022 to present</BioYear>
+					Works as Bioinformatics at the Institute of Bioorganics
+				</BioSection>
+			</Section>
+
+			<Section delay={0.3}>
+				<Heading as="h3" variant="section-title">
+					I 🖤
+				</Heading>
+				<Paragraph>
+					Art, Music, {' '}
+					<Link href="https://books.google.com/">
+						Reading
+					</Link>
+				</Paragraph>
+			</Section>
 		</Container>
 	)
 }
